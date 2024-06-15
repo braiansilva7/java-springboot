397cd4e48703684fff8671f1b29f137697a7a52a
Braian Silva <braian.diogenes@gmail.com>
2024-06-15 12:53:12 -0300

    Category

---
+++ src/main/java/com/aprendizado/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.aprendizado.course.repositories;
+
+import com.aprendizado.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
